cefa40f0b2a82d653b9ceeabc29e57fa5f3e8a18
Justina Wong <justinawong97@gmail.com>
2024-11-05 16:35:52 -0800

    link contact form with emailjs

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const dropdown = document.querySelector(".navLinks");
 const links = document.querySelectorAll(".navLinks a");
 const hero = document.querySelector("#hero");
 const backToTop = document.querySelector("#back-to-top");
+const BACKEND_API = "http://localhost:8080/submit"; // TO-DO: update this url after BE deployment
 
 const closeMenu = () => {
   menu.setAttribute("src", "./ui/icons/menu.svg");
@@ -29,7 +30,10 @@ menu.addEventListener("click", () => {
 });
 
 window.addEventListener("scroll", () => {
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 500) {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 500
+  ) {
     backToTop.classList.add("showBtn");
   } else {
     backToTop.classList.remove("showBtn");
@@ -40,3 +44,39 @@ backToTop.addEventListener("click", () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 });
+
+const setCurrentYear = () => {
+  const yearElement = document.getElementById("year");
+  yearElement.textContent = new Date().getFullYear();
+};
+
+document.addEventListener("DOMContentLoaded", setCurrentYear());
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData.entries());
+
+  try {
+    const response = await fetch(BACKEND_API, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    console.log(response);
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    } else {
+      setTimeout(() => {
+        window.location.href = "submitted.html";
+      }, 500);
+    }
+  } catch (e) {
+    throw new Error({ message: e.message });
+  }
+};
